8f0d5d2093f7acaaaa65ed78ec295511d4593bb2
ray2ker <engg.rayhan.sarker@gmail.com>
2017-03-15 00:49:15 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
    (function(window){'use strict';
-                     document.getElementById("weather").innerHTML=
-                       " 'hello how are you','how are you','hello'
-                        ";
+               var keywords = [
+               'hello how are you','how are you','hello',
+                  "please reload","reload",
+                  "please repeat","repeat",
+                  "thank you", 
+                  "good morning","hey",
+                  "tell me spanish",
+                  "tell me in Hindi",
+                  "good day",
+                  "good afternoon",
+                  "good night",
+                  "What time is it","Is too late","day of week","month of the year",
+                  "today's weather",
+                  "translate * in Spanish","translate * in german",
+                  "Pronunciate *",
+                  "Shut down yourself",
+                  'I love you','I love you so much','do you love me',
+                  'go to * '
+               ];
+                     document.getElementById("weather").innerHTML=keywords;
                      
                      
                    })(window);
